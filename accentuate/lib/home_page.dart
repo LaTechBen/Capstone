81e5e996f2721789dcda79cf61c42045349a12ab
UjjenRaj <ujjenrajkarnikar@gmail.com>
2024-01-08 11:54:48 -0600

    added profile images, title logo, story circle

--- accentuate/lib/home_page.dart
+++ accentuate/lib/home_page.dart
@@ -6,10 +6,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> profileImages = [
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg",
+    "images/7.jpg",
+    "images/8.jpg",
+    "images/9.jpg",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Image.asset(
+          "images/logo.png",
+          height: 50,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add_circle_outline),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_border),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.chat_bubble_outline),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          //Story
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(
+                8,
+                (index) => Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 35,
+                          backgroundImage: AssetImage(
+                            "images/story.png",
+                          ),
+                          child: CircleAvatar(
+                              radius: 32,
+                              backgroundImage: AssetImage(
+                                profileImages[index],
+                              )),
+                        ),
+                        Text(
+                          "Profile Name",
+                          style: TextStyle(fontSize: 12, color: Colors.black87),
+                        )
+                      ],
+                    )),
+              ),
+            ),
+          )
+          //Divider(),
+          //Here
+        ]),
+      ),
     );
   }
 }
