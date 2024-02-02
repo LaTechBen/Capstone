ac7c4d26500ed7e0f048e9dbd3347281eb85591f
LaTechBen <bjm056@email.latech.edu>
2024-02-01 20:44:31 -0600

    Post data program

---
+++ accentuate/lib/models/post.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Post {
+  final String description;
+  final String uid;
+  final String username;
+  final likes;
+  final String postID;
+  final DateTime datePublished;
+  final String postUrl;
+  final String profileImage;
+
+  const Post({
+    required this.description,
+    required this.uid,
+    required this.username,
+    required this.likes,
+    required this.postID,
+    required this.datePublished,
+    required this.postUrl,
+    required this.profileImage,
+  });
+
+  static Post fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+    return Post(
+      description: snapshot["description"],
+      uid: snapshot["uid"],
+      username: snapshot["username"],
+      likes: snapshot["likes"],
+      postID: snapshot["postID"],
+      datePublished: snapshot["datePublished"],
+      postUrl: snapshot["postUrl"],
+      profileImage: snapshot["profileImage"],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        "description": description,
+        "uid": uid,
+        "likes": likes,
+        "username": username,
+        "postID": postID,
+        "datePublished": datePublished,
+        'postUrl': postUrl,
+        'profileImage': profileImage
+      };
+}
